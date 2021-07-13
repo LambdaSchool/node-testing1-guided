7b73a227a77f9a0f14a961c38fb5cd18b529f0b9
Dave Kidd <david.kidd@lambdaschool.com>
2021-07-12 20:09:21 -0400

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -3,3 +3,9 @@ test("1st test",()=>{
     expect(3).not.toBe(4)
     expect(3).toBeGreaterThan(2)
 })
+test("is not the toEqual and toBe",()=>{
+    let a = {}
+    let b = {}
+    let c = a
+    expect(a).toBe(b)
+})
