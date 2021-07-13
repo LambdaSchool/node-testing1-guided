063c2f4e7ab81db4482857546b73074df0c49498
Dave Kidd <david.kidd@lambdaschool.com>
2021-07-12 21:07:28 -0400

    wip

--- car/car.js
+++ car/car.js
@@ -5,7 +5,7 @@ class Car {
         this.odometer = 0
     }
     drive(distance){
-        this.odometer = this.odometer + distance
+        this.odometer += distance
         return `${distance} miles driven`
     }
 }
